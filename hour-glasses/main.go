d7625214062354fde39bc894a529817123176ccf
isereb <ruxxay.coder@gmail.com>
2019-10-24 11:39:58 -0400

    Added runner and a use case

--- hour-glasses/main.go
+++ hour-glasses/main.go
@@ -1,4 +1,14 @@
-package hour_glasses
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // Complete the hourglassSum function below.
 // https://www.hackerrank.com/challenges/2d-array/problem
@@ -7,7 +17,7 @@ func hourglassSum(arr [][]int32) int32 {
 	rows := len(arr)
 	columns := len(arr[0])
 
-	var maxSum int32 = 0
+	var maxSum int32 = math.MinInt32
 	for i := 0; i < rows-2; i++ {
 		for j := 0; j < columns-2; j++ {
 			a1 := arr[i][j]
@@ -30,4 +40,56 @@ func hourglassSum(arr [][]int32) int32 {
 
 	return maxSum
 
+}
+
+func main() {
+	file, _ := os.Open("hour-glasses/input.txt")
+	reader := bufio.NewReaderSize(file, 1024*1024)
+
+	stdout, err := os.Create("hour-glasses/output.txt")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	var arr [][]int32
+	for i := 0; i < 6; i++ {
+		arrRowTemp := strings.Split(readLine(reader), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != int(6) {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := hourglassSum(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
